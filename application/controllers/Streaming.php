7727625fa93af46ef7e4c9d11332ec5b90cb8c8a
marcelpisowicz <marcel.pisowicz@gmail.com>
2017-10-05 13:03:18 +0200

    Testowanie klasy do streamowania wideo

--- application/controllers/Streaming.php
+++ application/controllers/Streaming.php
@@ -6,6 +6,10 @@ class Streaming extends Auth_Controller
 
     public function index($id)
     {
+        $this->load->helper('streaming');
+
+        $stream = new VideoStream("blob:https://player.webcamera.pl/1110d647-5fc2-4f6d-b3f4-89451ded7db2");
+        $stream->start();
         echo "Streaming, drone id =".$id;
     }
 }
